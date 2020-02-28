a6c0f05a65a5b51c561b93e60929237b60c46c52
Jesus Martinez <jmartinez@answare-tech.com>
2020-02-28 14:41:42 +0100

    Refactorize code

--- bot.py
+++ bot.py
@@ -2,14 +2,16 @@ import logging
 import emoji
 import datetime
 
-from telegram import InlineKeyboardButton, InlineKeyboardMarkup
-from telegram.ext import Updater, CommandHandler, CallbackQueryHandler
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup, Update
+from telegram.ext import Updater, CommandHandler, CallbackQueryHandler, CallbackContext
+from telegram.utils.promise import logger
 
 from config.authentication import API_TOKEN
 from utils.weather import weather, forecast
 from urllib.request import urlopen
 
-
+#https://github.com/python-telegram-bot/python-telegram-bot/wiki/Transition-guide-to-Version-12.0
+#https://github.com/python-telegram-bot/python-telegram-bot/wiki/Extensions-%E2%80%93-Your-first-Bot
 
 
 # Info para bots en Slack https://www.fullstackpython.com/blog/build-first-slack-bot-python.html
@@ -23,7 +25,7 @@ logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s
                     level=logging.INFO)
 
 
-def start(bot, update):
+def start(update: Update, context: CallbackContext):
     keyboard = [
                 [InlineKeyboardButton("Feliz", callback_data='1'),
                  InlineKeyboardButton("Whatever", callback_data='2')],
@@ -89,32 +91,31 @@ def help(bot, update):
     update.message.reply_text("Use /forecast City to obtain forecast in the next 3 hours for the City that you want")
 
 
-def error(bot, update, error):
-    logging.warning('Update "%s" caused error "%s"' % (update, error))
+def error_callback(update, context):
+    logger.warning('Update "%s" caused error "%s"', update, context.error)
 
-
-def getWeather(bot, update, args):
-    #em = emoji.emojize(':smile:', use_aliases=True)
-    print("Weather in "+str(args[0]))
-    eltiempo = weatherCity(str(args[0]))
-    bot.send_message(chat_id=update.message.chat_id, text=eltiempo)
-    print(eltiempo)
-
-def getForecast(bot, update, args):
-    # em = emoji.emojize(':smile:', use_aliases=True)
-    print("Forecast for the next hours in " + str(args[0]))
-    eltiempo = forecastCity(str(args[0]))
-    bot.send_message(chat_id=update.message.chat_id, text=eltiempo)
-    print(eltiempo)
+# def getWeather(bot, update, args):
+#     #em = emoji.emojize(':smile:', use_aliases=True)
+#     print("Weather in "+str(args[0]))
+#     eltiempo = weatherCity(str(args[0]))
+#     bot.send_message(chat_id=update.message.chat_id, text=eltiempo)
+#     print(eltiempo)
+#
+# def getForecast(update, context):
+#     # em = emoji.emojize(':smile:', use_aliases=True)
+#     print("Forecast for the next hours in " + str(args[0]))
+#     eltiempo = forecastCity(str(args[0]))
+#     bot.send_message(chat_id=update.message.chat_id, text=eltiempo)
+#     print(eltiempo)
 
 # Create the Updater and pass it your bot's token.
 updater = Updater(API_TOKEN, use_context=True)
 updater.dispatcher.add_handler(CommandHandler('start', start))
-updater.dispatcher.add_handler(CommandHandler('weather', getWeather, pass_args=True))
-updater.dispatcher.add_handler(CommandHandler('forecast', getForecast, pass_args=True))
+# updater.dispatcher.add_handler(CommandHandler('weather', self.getWeather, pass_args=True))
+# updater.dispatcher.add_handler(CommandHandler('forecast', self.getForecast, pass_args=True))
 updater.dispatcher.add_handler(CallbackQueryHandler(button))
 updater.dispatcher.add_handler(CommandHandler('help', help))
-updater.dispatcher.add_error_handler(error)
+updater.dispatcher.add_error_handler(error_callback)
 
 # Start the Bot
 updater.start_polling()
